0c3c17e575af8e4a1f034ce79a076818ed9518e4
aaryan200 <co21btech11001@iith.ac.in>
2024-04-22 00:29:07 +0530

    Created spinodal-2d comparison plot

--- aaryan/notebooks/utils.py
+++ aaryan/notebooks/utils.py
@@ -1,9 +1,12 @@
 import cunumeric as cun
 import numpy as np
+import pyfftw
+from scipy.fft import fft2 as scipy_fft2
+from scipy.fft import ifft2 as scipy_ifft2
 import matplotlib.pyplot as plt
-import time
 from tqdm import tqdm
 from IPython.display import clear_output
+from legate.timing import time
 
 def np_spinodal(arr_size = 100, num_iter = 10,
                  steps_per_iter = 100,
@@ -47,7 +50,7 @@ def np_spinodal(arr_size = 100, num_iter = 10,
     kappa = 1
 
     # Start time
-    start_time = time.time()
+    start_time = time("s")
 
     i_indices = np.arange(Nx) # Numbers from [0 to Nx-1]
     j_indices = np.arange(Ny) # Numbers from [0 to Ny-1]
@@ -84,7 +87,195 @@ def np_spinodal(arr_size = 100, num_iter = 10,
 
         if verbose: print(m)
 
-    end_time = time.time()
+    end_time = time("s")
+
+    if verbose:
+        clear_output(wait=True)
+        X,Y = np.meshgrid(range(Nx),range(Ny))        
+        plt.contourf(X,Y,c,cmap = 'jet')
+        plt.colorbar()
+        plt.show()
+
+    # Calculate and print the execution time
+    execution_time = end_time - start_time
+    if verbose:
+        print("Execution Time:", execution_time, "seconds")
+    return execution_time
+
+def sp_spinodal(arr_size = 100, num_iter = 10,
+                 steps_per_iter = 100,
+                verbose = True):
+    #size of the box
+    Nx = arr_size
+    Ny = arr_size
+    #Initial composition 
+    c = 0.5*np.ones([Nx,Ny])
+    #Noise - seed (mimic thermal fluctuations) 
+    np.random.seed(1024)
+    random_num = np.random.normal(0,0.01,(Nx,Ny))
+
+    c = c - random_num
+
+    if verbose:
+        X,Y = np.meshgrid(range(Nx),range(Ny))        
+        plt.contourf(X,Y,c,cmap = 'jet')
+        plt.colorbar()
+        plt.show()
+
+    dx = 1.0
+    dy = 1.0
+    dt = 0.5
+    cnew = c
+    #delkx is grid spacing along kx in Fourier space
+    #delky is grid spacing along ky in Fourier space
+
+    delkx = 2*np.pi/(Nx*dx)
+    delky = 2*np.pi/(Ny*dy)
+
+    #A is the prefactor of free energy f = A (c**2) * (1-c)**2
+    #A is inversely proportional to temperature
+
+    A = 1 
+
+    #M is the scaled constant mobility where diffusivity D = M (d^2f/dc^2) 
+    M = 1
+
+    # kappa is the scaled gradient energy coefficient (interfacial energy)
+    kappa = 1
+
+    # Start time
+    start_time = time("s")
+
+    i_indices = np.arange(Nx) # Numbers from [0 to Nx-1]
+    j_indices = np.arange(Ny) # Numbers from [0 to Ny-1]
+
+    kx = np.where(i_indices <= Nx/2, i_indices * delkx, (i_indices - Nx) * delkx)
+    # If i <= Nx/2, kx = i*delkx, else kx = (i-Nx)*delkx
+    ky = np.where(j_indices <= Ny/2, j_indices * delky, (j_indices - Ny) * delky)
+    # If j <= Ny/2, ky = j*delky, else ky = (j-Ny)*delky
+
+    # Calculate k2 and k4
+    # kx[:, np.newaxis] will reshape it to (Nx, 1)
+    # ky[np.newaxis, :] will reshape it to (1, Ny)
+    # Their squared addition will result in a (Nx, Ny) matrix
+    # k2[first row] = [kx[0]**2 + ky[0]**2, kx[0]**2 + ky[1]**2, ..., kx[0]**2 + ky[Ny-1]**2]
+    k2 = kx[:, np.newaxis]**2 + ky[np.newaxis, :]**2
+    k4 = k2**2
+
+    # Outer iterations
+    for m in range(num_iter):
+        # Inner iterations
+        for n in range(steps_per_iter):
+            
+            #g stores the first derivative of free energy
+            #df/dc = 2*A*c*(1-c)*(1-2c)
+            mult = np.multiply(1-cnew,1-2*cnew)
+            g = 2*A*np.multiply(cnew,mult)
+            ghat = scipy_fft2(g)
+            chat = scipy_fft2(cnew)
+            
+            chat = (chat - M*dt*k2*ghat)/(1+2*M*kappa*k4*dt)
+
+            cnew = scipy_ifft2(chat).real
+            c = cnew
+
+        if verbose: print(m)
+
+    end_time = time("s")
+
+    if verbose:
+        clear_output(wait=True)
+        X,Y = np.meshgrid(range(Nx),range(Ny))        
+        plt.contourf(X,Y,c,cmap = 'jet')
+        plt.colorbar()
+        plt.show()
+
+    # Calculate and print the execution time
+    execution_time = end_time - start_time
+    if verbose:
+        print("Execution Time:", execution_time, "seconds")
+    return execution_time
+
+def pyfftw_spinodal(arr_size = 100, num_iter = 10,
+                 steps_per_iter = 100,
+                verbose = True):
+    #size of the box
+    Nx = arr_size
+    Ny = arr_size
+    #Initial composition 
+    c = 0.5*np.ones([Nx,Ny])
+    #Noise - seed (mimic thermal fluctuations) 
+    np.random.seed(1024)
+    random_num = np.random.normal(0,0.01,(Nx,Ny))
+
+    c = c - random_num
+
+    if verbose:
+        X,Y = np.meshgrid(range(Nx),range(Ny))        
+        plt.contourf(X,Y,c,cmap = 'jet')
+        plt.colorbar()
+        plt.show()
+
+    dx = 1.0
+    dy = 1.0
+    dt = 0.5
+    cnew = c
+    #delkx is grid spacing along kx in Fourier space
+    #delky is grid spacing along ky in Fourier space
+
+    delkx = 2*np.pi/(Nx*dx)
+    delky = 2*np.pi/(Ny*dy)
+
+    #A is the prefactor of free energy f = A (c**2) * (1-c)**2
+    #A is inversely proportional to temperature
+
+    A = 1 
+
+    #M is the scaled constant mobility where diffusivity D = M (d^2f/dc^2) 
+    M = 1
+
+    # kappa is the scaled gradient energy coefficient (interfacial energy)
+    kappa = 1
+
+    # Start time
+    start_time = time("s")
+
+    i_indices = np.arange(Nx) # Numbers from [0 to Nx-1]
+    j_indices = np.arange(Ny) # Numbers from [0 to Ny-1]
+
+    kx = np.where(i_indices <= Nx/2, i_indices * delkx, (i_indices - Nx) * delkx)
+    # If i <= Nx/2, kx = i*delkx, else kx = (i-Nx)*delkx
+    ky = np.where(j_indices <= Ny/2, j_indices * delky, (j_indices - Ny) * delky)
+    # If j <= Ny/2, ky = j*delky, else ky = (j-Ny)*delky
+
+    # Calculate k2 and k4
+    # kx[:, np.newaxis] will reshape it to (Nx, 1)
+    # ky[np.newaxis, :] will reshape it to (1, Ny)
+    # Their squared addition will result in a (Nx, Ny) matrix
+    # k2[first row] = [kx[0]**2 + ky[0]**2, kx[0]**2 + ky[1]**2, ..., kx[0]**2 + ky[Ny-1]**2]
+    k2 = kx[:, np.newaxis]**2 + ky[np.newaxis, :]**2
+    k4 = k2**2
+
+    # Outer iterations
+    for m in range(num_iter):
+        # Inner iterations
+        for n in range(steps_per_iter):
+            
+            #g stores the first derivative of free energy
+            #df/dc = 2*A*c*(1-c)*(1-2c)
+            mult = np.multiply(1-cnew,1-2*cnew)
+            g = 2*A*np.multiply(cnew,mult)
+            ghat = pyfftw.interfaces.numpy_fft.fft2(g)
+            chat = pyfftw.interfaces.numpy_fft.fft2(cnew)
+            
+            chat = (chat - M*dt*k2*ghat)/(1+2*M*kappa*k4*dt)
+
+            cnew = pyfftw.interfaces.numpy_fft.ifft2(chat).real
+            c = cnew
+
+        if verbose: print(m)
+
+    end_time = time("s")
 
     if verbose:
         clear_output(wait=True)
@@ -114,7 +305,7 @@ def cun_spinodal(arr_size = 100, num_iter = 10,
     c = c - random_num
 
     if verbose:
-        X,Y = cun.meshgrid(range(Nx),range(Ny))        
+        X,Y = np.meshgrid(range(Nx),range(Ny))        
         plt.contourf(X,Y,c,cmap = 'jet')
         plt.colorbar()
         plt.show()
@@ -141,7 +332,7 @@ def cun_spinodal(arr_size = 100, num_iter = 10,
     kappa = 1
 
     # Start time
-    start_time = time.time()
+    start_time = time("s")
 
     i_indices = cun.arange(Nx) # Numbers from [0 to Nx-1]
     j_indices = cun.arange(Ny) # Numbers from [0 to Ny-1]
@@ -177,11 +368,11 @@ def cun_spinodal(arr_size = 100, num_iter = 10,
 
         if verbose: print(m)
 
-    end_time = time.time()
+    end_time = time("s")
 
     if verbose:
         clear_output(wait=True)
-        X,Y = cun.meshgrid(range(Nx),range(Ny))        
+        X,Y = np.meshgrid(range(Nx),range(Ny))        
         plt.contourf(X,Y,c,cmap = 'jet')
         plt.colorbar()
         plt.show()
@@ -191,6 +382,7 @@ def cun_spinodal(arr_size = 100, num_iter = 10,
     if verbose:
         print("Execution Time:", execution_time, "seconds")
     return execution_time
+
 
 def plot_graph(times, labels, sizes, title,
                x_lim = None,
